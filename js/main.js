d68ea192a4844619eb14bfa2dcae5a8e14039944
Sarah <143218168+Sarah-S-R@users.noreply.github.com>
2023-11-13 15:33:18 -0330

    success message

--- js/main.js
+++ js/main.js
@@ -78,85 +78,6 @@ if (form) {
     const validator = new Login(form, fields);
 }
 
-//Signup Form
-/*class Signup {
-	constructor(form, fields) {
-		this.form = form;
-		this.fields = fields;
-		this.validateonSubmit();
-	}
-
-	validateonSubmit() {
-        let self = this;
-    
-        this.form.addEventListener("submit", (e) => {
-            e.preventDefault();
-            var error = 0;
-            self.fields.forEach((field) => {
-                const input = document.querySelector(`#${field}`);
-                if (self.validateFields(input) === false) {
-                    error++;
-                }
-            });
-            if (error === 0) {
-                //do login api here
-				localStorage.setItem("auth", 1);
-                this.form.submit(); // This should trigger the form submission.
-            }
-        });
-    }
-    
-        validateFields(field) {
-            if (field.value.trim() === "") {
-                this.setStatus(
-                    field,
-                    `${field.previousElementSibling.innerText} cannot be blank`,
-                    "error"
-                );
-                return false;
-            } else {
-                if (field.type == "password") {
-                    if (field.value.length < 8) {
-                        this.setStatus(
-                            field,
-                            `${field.previousElementSibling.innerText} must be at least 8 characters`,
-                            "error"
-                        );
-                        return false;
-                    } else {
-                        this.setStatus(field, null, "success");
-                        return true;
-                    }
-                } else {
-                    this.setStatus(field, null, "success");
-                    return true;
-                }
-            }
-        }
-    
-        setStatus(field, message, status) {
-            console.log("setStatus function is called")
-            const errorMessage = field.parentElement.querySelector(".error-message");
-    
-            if (status == "success") {
-                if (errorMessage) {
-                    errorMessage.innerText = "";
-                }
-                field.classList.remove("input-error");
-            }
-    
-            if (status == "error") {
-                errorMessage.innerText = message;
-                field.classList.add("input-error");
-            }
-        }
-    }
-
-const createAccountForm = document.querySelector(".create-account-form");
-if (createAccountForm) {
-    const fields= ["email", "password", "fname", "surname"];
-    const validator = new Signup(form, fields);
-}*/
 
 const createAccountForm = document.getElementById ("create-account-form");
 const username = document.getElementById ('username');
@@ -164,6 +85,7 @@ const email = document.getElementById ('email');
 const password = document.getElementById ('password');
 const passwordTwo = document.getElementById ('passwordTwo');
 const errorElement = document.getElementById ('error-message');
+const successMessage = document.getElementById ('success-message');
 
     createAccountForm.addEventListener ('submit', (e) => {
               
@@ -196,4 +118,23 @@ const errorElement = document.getElementById ('error-message');
         e.preventDefault()
         errorElement.innerText = messages.join('; ')
     }
-    })
+
+    if (messages.length === 0) {
+        // Clear error message
+        errorElement.innerText = '';
+
+        // Display success message
+        successMessage.innerText = 'Account created successfully!';
+        successMessage.style.color = 'green'; // You can customize the style
+
+        // You may also want to redirect or perform additional actions here
+        // Redirect after a short delay (e.g., 2 seconds)
+    setTimeout(() => {
+        window.location.href = '/dashboard.html'; // Replace with your desired URL
+    }, 3000); // Adjust the delay time as needed
+
+    } else {
+        // Display error messages
+        errorElement.innerText = messages.join('; ');
+    }
+});
