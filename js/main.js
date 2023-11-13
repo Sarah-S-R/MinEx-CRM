a6912d81b5bcc53d5cc3703f63cbcfcfd337db3e
Sarah <143218168+Sarah-S-R@users.noreply.github.com>
2023-11-13 14:57:25 -0330

    validation

--- js/main.js
+++ js/main.js
@@ -176,6 +176,22 @@ const errorElement = document.getElementById ('error-message');
         messages.push('Password must be at least 8 characters')
     }
 
+    if(password.value.length >=20) {
+        messages.push('Password must be less than 20 characters')
+    }
+
+    if(password.value === 'password') {
+        messages.push('Password cannot be password')
+    }
+
+    if(!/\d/.test(password.value)) {
+        messages.push('Password must contain at least one number')
+    }
+
+    if(password.value !=passwordTwo.value) {
+        messages.push('Passwords do not match')
+    }
+
     if(messages.length > 0) {
         e.preventDefault()
         errorElement.innerText = messages.join('; ')
