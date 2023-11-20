17cbdbf8709f03d75bf46d77448d18c127d7dedc
Sarah <143218168+Sarah-S-R@users.noreply.github.com>
2023-11-20 18:43:10 -0330

    created new prj files

---
+++ js/old-projects.js
@@ -0,0 +1,235 @@
+
+const tableKey = 'pms-table';
+let pmsTable;
+let pmsTableDemo = {};
+
+document.getElementById('pmSortButton').addEventListener('click', () => {
+    const sortedKeys = Object.keys(pmsTable).sort((a, b) => {
+        // Convert keys to lowercase for case-insensitive sorting
+        const keyA = a.toLowerCase();
+        const keyB = b.toLowerCase();
+
+        // Compare keys as numbers if both are numeric, otherwise, compare as strings
+        if (!isNaN(keyA) && !isNaN(keyB)) {
+            return parseFloat(keyA) - parseFloat(keyB);
+        } else {
+            return keyA.localeCompare(keyB);
+        }
+    });
+
+    const tempTable = {};
+    sortedKeys.forEach(key => (tempTable[key] = pmsTable[key]));
+    pmsTable = tempTable;
+    refreshTable();
+});
+
+let enableDisableCompanyInput = (option) => {
+    let newProjectCompany = document.getElementById('newProjectCompany');
+    
+    if (option === 'enable')
+        newProjectCompany.disabled = false;
+   
+        else if (option === 'disable')
+        newProjectCompany.disabled = true;
+}
+
+let refreshTable = () => { 
+    let pmsTableKeys = Object.keys(pmsTable);
+    let tableContainer = document.getElementById('pmsTableContainer');
+    let oldTableBody = document.getElementById('pmTableBody');
+   
+    tableContainer.removeChild(oldTableBody);
+   
+    let newTableBody = document.createElement('span');
+    newTableBody.id = 'pmTableBody';
+    tableContainer.appendChild(newTableBody);
+    
+    for(let i = 0; i < pmsTableKeys.length;i++){
+        let currentRow = document.createElement('div');
+        let currentCompanyCol = document.createElement('div');
+        let currentPropertyNameCol = document.createElement('div');
+        let currentLocationCol = document.createElement('div');
+        let currentClaimsCol = document.createElement('div');
+        let currentAreaCol = document.createElement('div');
+        let currentEditBtn = document.createElement('div');
+        let currentDeleteBtn = document.createElement('div');
+
+
+        currentRow.className = 'pm-table-row';
+        currentCompanyCol.className = 'pm-table-column pm-company';
+        currentPropertyNameCol.className = 'pm-table-column pm-propertyName';
+        currentLocationCol.className = 'pm-table-column pm-location';
+        currentClaimsCol.className = 'pm-table-column pm-claims';
+        currentAreaCol.className = 'pm-table-column pm-area';
+        currentEditBtn.className = 'pm-table-column pm-edit';
+        currentDeleteBtn.className = 'pm-table-column pm-delete';
+
+        currentCompanyCol.innerHTML = pmsTableKeys[i];
+        currentPropertyNameCol.innerHTML = pmsTable[pmsTableKeys[i]].propertyName;
+        currentLocationCol.innerHTML = pmsTable[pmsTableKeys[i]].location;
+        currentClaimsCol.innerHTML = pmsTable[pmsTableKeys[i]].claims;
+        currentAreaCol.innerHTML = pmsTable[pmsTableKeys[i]].area;
+
+        currentDeleteBtn.innerHTML = '<i class="fas fa-dumpster"></i>';
+        currentEditBtn.innerHTML = '<i class="fas fa-user-edit"></i>';
+
+        currentRow.appendChild(currentCompanyCol);
+        currentRow.appendChild(currentPropertyNameCol);
+        currentRow.appendChild(currentLocationCol);
+        currentRow.appendChild(currentClaimsCol);
+        currentRow.appendChild(currentAreaCol);
+        currentRow.appendChild(currentEditBtn);
+        currentRow.appendChild(currentDeleteBtn);
+        newTableBody.appendChild(currentRow);
+    }
+
+    let enableDisableNewUserModal = (option) => {
+       
+        let newProjectCompany = document.getElementById('newProjectCompany');
+        let newProjectPropertyName = document.getElementById('newProjectPropertyName');
+        let newProjectLocation = document.getElementById('newProjectLocation');
+        let newProjectClaims = document.getElementById('newProjectClaims');
+        let newProjectArea = document.getElementById('newProjectArea');
+        
+        newProjectCompany.value = '';
+        newProjectPropertyName.value = '';
+        newProjectLocation.value = '';
+        newProjectClaims.value = '';
+        newProjectArea.value = '';
+        
+        let newProjectModal = document.getElementById('newProjectModal');
+        let pmbackdrop = document.getElementById('pmbackdrop');
+        
+        newProjectModal.className = `${option}-modal`;
+        pmbackdrop.className = `${option}-modal`;
+    }
+
+    let addNewEntryBtn = document.getElementById('pmAddNewEntry');
+    let editBtns = document.getElementsByClassName('pm-edit');
+    let deleteBtns = document.getElementsByClassName('pm-delete');
+    let newProjectSubmitBtn = document.getElementById('newProjectSubmitButton');
+    let newProjectCancelBtn = document.getElementById('newProjectCancelButton');
+    
+    newProjectSubmitBtn.addEventListener('click', () => {
+        
+        let newProjectCompany = document.getElementById('newProjectCompany').value.trim();
+        let newProjectPropertyName = document.getElementById('newProjectPropertyName').value.trim();
+        let newProjectLocation = document.getElementById('newProjectLocation').value.trim();
+        let newProjectClaims = document.getElementById('newProjectClaims').value.trim();
+        let newProjectArea = document.getElementById('newProjectArea').value.trim();
+        
+        if(newProjectCompany === '')
+            document.getElementById('newProjectCompany').className = 'input-err';
+        
+            else 
+            document.getElementById('newProjectCompany').className = '';
+
+        if(newProjectPropertyName === '')
+            document.getElementById('newProjectPropertyName').className = 'input-err';
+        
+        else 
+            document.getElementById('newProjectPropertyName').className = '';
+       
+        if(newProjectLocation === '')
+            document.getElementById('newProjectLocation').className = 'input-err';
+        
+        else 
+            document.getElementById('newProjectLocation').className = '';
+        
+        if(newProjectClaims === '')
+            document.getElementById('newProjectClaims').className = 'input-err';
+        
+        else 
+            document.getElementById('newProjectClaims').className = '';
+       
+         if(newProjectArea === '')
+            document.getElementById('newProjectArea').className = 'input-err';
+       
+        else 
+            document.getElementById('newProjectArea').className = '';
+        
+        
+        if(newProjectCompany !== '' && newProjectPropertyName !== '' && newProjectLocation !== '' && newProjectClaims !== '' && newProjectArea !== ''){
+            pmsTable[newProjectCompany] = {
+                
+                'propertyName': newProjectPropertyName,
+                'location': newProjectLocation,
+                'claims': newProjectClaims,
+                'area': newProjectArea,
+            }
+            localStorage.setItem(tableKey,JSON.stringify(pmsTable));
+            enableDisableNewUserModal('disable');
+            refreshTable();
+        }
+    });
+   
+    newProjectCancelBtn.addEventListener('click', () =>{
+        enableDisableNewUserModal('disable');  
+    });
+    
+    addNewEntryBtn.addEventListener('click', () => {
+        enableDisableNewUserModal('enable');
+    });
+   
+    for(let i = 0; i < editBtns.length; i++){
+       
+        editBtns[i].addEventListener('click', ($event) => {
+           
+            let prjnameToEdit = $event.target.parentElement.children[0].innerText;
+            let projectToEdit = pmsTable[prjnameToEdit];
+            
+            enableDisableCompanyInput('enable');
+            enableDisableNewUserModal('enable');
+           
+            let newProjectCompany = document.getElementById('newProjectCompany');
+            let newProjectPropertyName = document.getElementById('newProjectPropertyName');
+            let newProjectLocation = document.getElementById('newProjectLocation');
+            let newProjectClaims = document.getElementById('newProjectClaims');
+            let newProjectArea = document.getElementById('newProjectArea');
+           
+            
+            newProjectCompany.value = prjnameToEdit; //use company name for editing
+            newProjectPropertyName.value = projectToEdit.propertyName;
+            newProjectLocation.value = projectToEdit.location;
+            newProjectClaims.value = projectToEdit.claims;
+            newProjectArea.value = projectToEdit.area;
+        });
+    }
+    for(let i = 0; i < deleteBtns.length; i++){
+        deleteBtns[i].addEventListener('click', ($event) => {
+            
+            let nameToDelete = $event.target.parentElement.children[0].innerText;
+            let isSure = window.confirm('Are you sure you want to delete ' + nameToDelete + '?');
+            
+            if(isSure)
+                // delete user from table
+                deleteUserFromTable(nameToDelete);
+        })
+    }
+}
+let deleteUserFromTable = (userName) => {
+    let tempTable = {};
+    let pmsTableKeys = Object.keys(pmsTable);
+    
+    for(let i = 0; i < pmsTableKeys.length; i++){
+        if(userName !== pmsTableKeys[i]){
+            tempTable[pmsTableKeys[i]] = pmsTable[pmsTableKeys[i]]; 
+        }
+    }
+    pmsTable = tempTable;
+    localStorage.setItem(tableKey,JSON.stringify(pmsTable));
+    refreshTable();
+}
+let init = () => {
+    
+    if(localStorage.getItem(tableKey)){
+        pmsTable = JSON.parse(localStorage.getItem(tableKey));
+    }
+    
+    else {
+        pmsTable = pmsTableDemo;
+        localStorage.setItem(tableKey,JSON.stringify(pmsTable));
+    }
+    refreshTable();
+}
+init();
--- js/projects.js
+++ js/projects.js
@@ -1,235 +1,270 @@
+// SIDEBAR TOGGLE
+let sidebarOpen=false;
+let sidebar = document.getElementById("sidebar");
 
-const tableKey = 'pms-table';
-let pmsTable;
-let pmsTableDemo = {};
-
-document.getElementById('pmSortButton').addEventListener('click', () => {
-    const sortedKeys = Object.keys(pmsTable).sort((a, b) => {
-        // Convert keys to lowercase for case-insensitive sorting
-        const keyA = a.toLowerCase();
-        const keyB = b.toLowerCase();
-
-        // Compare keys as numbers if both are numeric, otherwise, compare as strings
-        if (!isNaN(keyA) && !isNaN(keyB)) {
-            return parseFloat(keyA) - parseFloat(keyB);
-        } else {
-            return keyA.localeCompare(keyB);
-        }
-    });
-
-    const tempTable = {};
-    sortedKeys.forEach(key => (tempTable[key] = pmsTable[key]));
-    pmsTable = tempTable;
-    refreshTable();
-});
-
-let enableDisableCompanyInput = (option) => {
-    let newProjectCompany = document.getElementById('newProjectCompany');
-    
-    if (option === 'enable')
-        newProjectCompany.disabled = false;
-   
-        else if (option === 'disable')
-        newProjectCompany.disabled = true;
-}
-
-let refreshTable = () => { 
-    let pmsTableKeys = Object.keys(pmsTable);
-    let tableContainer = document.getElementById('pmsTableContainer');
-    let oldTableBody = document.getElementById('pmTableBody');
-   
-    tableContainer.removeChild(oldTableBody);
-   
-    let newTableBody = document.createElement('span');
-    newTableBody.id = 'pmTableBody';
-    tableContainer.appendChild(newTableBody);
-    
-    for(let i = 0; i < pmsTableKeys.length;i++){
-        let currentRow = document.createElement('div');
-        let currentCompanyCol = document.createElement('div');
-        let currentPropertyNameCol = document.createElement('div');
-        let currentLocationCol = document.createElement('div');
-        let currentClaimsCol = document.createElement('div');
-        let currentAreaCol = document.createElement('div');
-        let currentEditBtn = document.createElement('div');
-        let currentDeleteBtn = document.createElement('div');
-
-
-        currentRow.className = 'pm-table-row';
-        currentCompanyCol.className = 'pm-table-column pm-company';
-        currentPropertyNameCol.className = 'pm-table-column pm-propertyName';
-        currentLocationCol.className = 'pm-table-column pm-location';
-        currentClaimsCol.className = 'pm-table-column pm-claims';
-        currentAreaCol.className = 'pm-table-column pm-area';
-        currentEditBtn.className = 'pm-table-column pm-edit';
-        currentDeleteBtn.className = 'pm-table-column pm-delete';
-
-        currentCompanyCol.innerHTML = pmsTableKeys[i];
-        currentPropertyNameCol.innerHTML = pmsTable[pmsTableKeys[i]].propertyName;
-        currentLocationCol.innerHTML = pmsTable[pmsTableKeys[i]].location;
-        currentClaimsCol.innerHTML = pmsTable[pmsTableKeys[i]].claims;
-        currentAreaCol.innerHTML = pmsTable[pmsTableKeys[i]].area;
-
-        currentDeleteBtn.innerHTML = '<i class="fas fa-dumpster"></i>';
-        currentEditBtn.innerHTML = '<i class="fas fa-user-edit"></i>';
-
-        currentRow.appendChild(currentCompanyCol);
-        currentRow.appendChild(currentPropertyNameCol);
-        currentRow.appendChild(currentLocationCol);
-        currentRow.appendChild(currentClaimsCol);
-        currentRow.appendChild(currentAreaCol);
-        currentRow.appendChild(currentEditBtn);
-        currentRow.appendChild(currentDeleteBtn);
-        newTableBody.appendChild(currentRow);
-    }
-
-    let enableDisableNewUserModal = (option) => {
-       
-        let newProjectCompany = document.getElementById('newProjectCompany');
-        let newProjectPropertyName = document.getElementById('newProjectPropertyName');
-        let newProjectLocation = document.getElementById('newProjectLocation');
-        let newProjectClaims = document.getElementById('newProjectClaims');
-        let newProjectArea = document.getElementById('newProjectArea');
-        
-        newProjectCompany.value = '';
-        newProjectPropertyName.value = '';
-        newProjectLocation.value = '';
-        newProjectClaims.value = '';
-        newProjectArea.value = '';
-        
-        let newProjectModal = document.getElementById('newProjectModal');
-        let pmbackdrop = document.getElementById('pmbackdrop');
-        
-        newProjectModal.className = `${option}-modal`;
-        pmbackdrop.className = `${option}-modal`;
-    }
-
-    let addNewEntryBtn = document.getElementById('pmAddNewEntry');
-    let editBtns = document.getElementsByClassName('pm-edit');
-    let deleteBtns = document.getElementsByClassName('pm-delete');
-    let newProjectSubmitBtn = document.getElementById('newProjectSubmitButton');
-    let newProjectCancelBtn = document.getElementById('newProjectCancelButton');
-    
-    newProjectSubmitBtn.addEventListener('click', () => {
-        
-        let newProjectCompany = document.getElementById('newProjectCompany').value.trim();
-        let newProjectPropertyName = document.getElementById('newProjectPropertyName').value.trim();
-        let newProjectLocation = document.getElementById('newProjectLocation').value.trim();
-        let newProjectClaims = document.getElementById('newProjectClaims').value.trim();
-        let newProjectArea = document.getElementById('newProjectArea').value.trim();
-        
-        if(newProjectCompany === '')
-            document.getElementById('newProjectCompany').className = 'input-err';
-        
-            else 
-            document.getElementById('newProjectCompany').className = '';
-
-        if(newProjectPropertyName === '')
-            document.getElementById('newProjectPropertyName').className = 'input-err';
-        
-        else 
-            document.getElementById('newProjectPropertyName').className = '';
-       
-        if(newProjectLocation === '')
-            document.getElementById('newProjectLocation').className = 'input-err';
-        
-        else 
-            document.getElementById('newProjectLocation').className = '';
-        
-        if(newProjectClaims === '')
-            document.getElementById('newProjectClaims').className = 'input-err';
-        
-        else 
-            document.getElementById('newProjectClaims').className = '';
-       
-         if(newProjectArea === '')
-            document.getElementById('newProjectArea').className = 'input-err';
-       
-        else 
-            document.getElementById('newProjectArea').className = '';
-        
-        
-        if(newProjectCompany !== '' && newProjectPropertyName !== '' && newProjectLocation !== '' && newProjectClaims !== '' && newProjectArea !== ''){
-            pmsTable[newProjectCompany] = {
-                
-                'propertyName': newProjectPropertyName,
-                'location': newProjectLocation,
-                'claims': newProjectClaims,
-                'area': newProjectArea,
-            }
-            localStorage.setItem(tableKey,JSON.stringify(pmsTable));
-            enableDisableNewUserModal('disable');
-            refreshTable();
-        }
-    });
-   
-    newProjectCancelBtn.addEventListener('click', () =>{
-        enableDisableNewUserModal('disable');  
-    });
-    
-    addNewEntryBtn.addEventListener('click', () => {
-        enableDisableNewUserModal('enable');
-    });
-   
-    for(let i = 0; i < editBtns.length; i++){
-       
-        editBtns[i].addEventListener('click', ($event) => {
-           
-            let prjnameToEdit = $event.target.parentElement.children[0].innerText;
-            let projectToEdit = pmsTable[prjnameToEdit];
-            
-            enableDisableCompanyInput('enable');
-            enableDisableNewUserModal('enable');
-           
-            let newProjectCompany = document.getElementById('newProjectCompany');
-            let newProjectPropertyName = document.getElementById('newProjectPropertyName');
-            let newProjectLocation = document.getElementById('newProjectLocation');
-            let newProjectClaims = document.getElementById('newProjectClaims');
-            let newProjectArea = document.getElementById('newProjectArea');
-           
-            
-            newProjectCompany.value = prjnameToEdit; //use company name for editing
-            newProjectPropertyName.value = projectToEdit.propertyName;
-            newProjectLocation.value = projectToEdit.location;
-            newProjectClaims.value = projectToEdit.claims;
-            newProjectArea.value = projectToEdit.area;
-        });
-    }
-    for(let i = 0; i < deleteBtns.length; i++){
-        deleteBtns[i].addEventListener('click', ($event) => {
-            
-            let nameToDelete = $event.target.parentElement.children[0].innerText;
-            let isSure = window.confirm('Are you sure you want to delete ' + nameToDelete + '?');
-            
-            if(isSure)
-                // delete user from table
-                deleteUserFromTable(nameToDelete);
-        })
+function openSidebar () {
+    if (!sidebarOpen) {
+        sidebar.classList.add("sidebar-responsive");
+        sidebarOpen =true;
     }
 }
-let deleteUserFromTable = (userName) => {
-    let tempTable = {};
-    let pmsTableKeys = Object.keys(pmsTable);
-    
-    for(let i = 0; i < pmsTableKeys.length; i++){
-        if(userName !== pmsTableKeys[i]){
-            tempTable[pmsTableKeys[i]] = pmsTable[pmsTableKeys[i]]; 
-        }
+
+function closeSidebar () {
+    if (sidebarOpen) {
+        sidebar.classList.remove("sidebar-responsive");
+        sidebarOpen = false;
+        console.log ("Close sidebar clicked");
     }
-    pmsTable = tempTable;
-    localStorage.setItem(tableKey,JSON.stringify(pmsTable));
-    refreshTable();
 }
-let init = () => {
-    
-    if(localStorage.getItem(tableKey)){
-        pmsTable = JSON.parse(localStorage.getItem(tableKey));
-    }
-    
-    else {
-        pmsTable = pmsTableDemo;
-        localStorage.setItem(tableKey,JSON.stringify(pmsTable));
-    }
-    refreshTable();
+
+function updateDateTime() {
+  const datetimeContainer = document.getElementById('datetime');
+  const now = new Date();
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+  const formattedDate = now.toLocaleDateString('en-US', options);
+  datetimeContainer.textContent = formattedDate;
 }
-init();
+
+// Update date and time every second
+setInterval(updateDateTime, 1000);
+
+// Initial update
+updateDateTime();
+
+
+//---------CHARTS------------
+// BAR CHART
+var barChartOptions = {
+    series: [
+      {
+        data: [10, 8, 6, 4, 2],
+        name: 'Services',
+      },
+    ],
+    chart: {
+      type: 'bar',
+      background: 'transparent',
+      height: 350,
+      toolbar: {
+        show: false,
+      },
+    },
+    colors: ['#2962ff', '#d50000', '#2e7d32', '#ff6d00', '#583cb3'],
+    plotOptions: {
+      bar: {
+        distributed: true,
+        borderRadius: 4,
+        horizontal: false,
+        columnWidth: '40%',
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    fill: {
+      opacity: 1,
+    },
+    grid: {
+      borderColor: '#55596e',
+      yaxis: {
+        lines: {
+          show: true,
+        },
+      },
+      xaxis: {
+        lines: {
+          show: true,
+        },
+      },
+    },
+    legend: {
+      labels: {
+        colors: '#f5f7ff',
+      },
+      show: true,
+      position: 'top',
+    },
+    stroke: {
+      colors: ['transparent'],
+      show: true,
+      width: 2,
+    },
+    tooltip: {
+      shared: true,
+      intersect: false,
+      theme: 'dark',
+    },
+    xaxis: {
+      categories: ['Fieldwork', 'Geophysics', 'Claims Mgmt', 'Reporting', 'AAA'],
+      title: {
+        style: {
+          color: '#f5f7ff',
+        },
+      },
+      axisBorder: {
+        show: true,
+        color: '#55596e',
+      },
+      axisTicks: {
+        show: true,
+        color: '#55596e',
+      },
+      labels: {
+        style: {
+          colors: '#f5f7ff',
+        },
+      },
+    },
+    yaxis: {
+      title: {
+        text: 'Count',
+        style: {
+          color: '#f5f7ff',
+        },
+      },
+      axisBorder: {
+        color: '#55596e',
+        show: true,
+      },
+      axisTicks: {
+        color: '#55596e',
+        show: true,
+      },
+      labels: {
+        style: {
+          colors: '#f5f7ff',
+        },
+      },
+    },
+  };
+  
+  var barChart = new ApexCharts(
+    document.querySelector('#bar-chart'),
+    barChartOptions
+  );
+  barChart.render();
+
+  // AREA CHART
+var areaChartOptions = {
+    series: [
+      {
+        name: 'Purchase Orders',
+        data: [31, 40, 28, 51, 42, 109, 100],
+      },
+      {
+        name: 'Sales Orders',
+        data: [11, 32, 45, 32, 34, 52, 41],
+      },
+    ],
+    chart: {
+      type: 'area',
+      background: 'transparent',
+      height: 350,
+      stacked: false,
+      toolbar: {
+        show: false,
+      },
+    },
+    colors: ['#00ab57', '#d50000'],
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+    dataLabels: {
+      enabled: false,
+    },
+    fill: {
+      gradient: {
+        opacityFrom: 0.4,
+        opacityTo: 0.1,
+        shadeIntensity: 1,
+        stops: [0, 100],
+        type: 'vertical',
+      },
+      type: 'gradient',
+    },
+    grid: {
+      borderColor: '#55596e',
+      yaxis: {
+        lines: {
+          show: true,
+        },
+      },
+      xaxis: {
+        lines: {
+          show: true,
+        },
+      },
+    },
+    legend: {
+      labels: {
+        colors: '#f5f7ff',
+      },
+      show: true,
+      position: 'top',
+    },
+    markers: {
+      size: 6,
+      strokeColors: '#1b2635',
+      strokeWidth: 3,
+    },
+    stroke: {
+      curve: 'smooth',
+    },
+    xaxis: {
+      axisBorder: {
+        color: '#55596e',
+        show: true,
+      },
+      axisTicks: {
+        color: '#55596e',
+        show: true,
+      },
+      labels: {
+        offsetY: 5,
+        style: {
+          colors: '#f5f7ff',
+        },
+      },
+    },
+    yaxis: [
+      {
+        title: {
+          text: 'Purchase Orders',
+          style: {
+            color: '#f5f7ff',
+          },
+        },
+        labels: {
+          style: {
+            colors: ['#f5f7ff'],
+          },
+        },
+      },
+      {
+        opposite: true,
+        title: {
+          text: 'Sales Orders',
+          style: {
+            color: '#f5f7ff',
+          },
+        },
+        labels: {
+          style: {
+            colors: ['#f5f7ff'],
+          },
+        },
+      },
+    ],
+    tooltip: {
+      shared: true,
+      intersect: false,
+      theme: 'dark',
+    },
+  };
+  
+  var areaChart = new ApexCharts(
+    document.querySelector('#area-chart'),
+    areaChartOptions
+  );
+  areaChart.render();
+  
+
+  
